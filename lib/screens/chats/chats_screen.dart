de53e201d43ca2e2fab0abe77d17cc0b18f0a5ca
DorseyCn <nwakaemmanuelchimezie@gmail.com>
2021-03-25 06:30:06 -0700

    clean

--- lib/screens/chats/chats_screen.dart
+++ lib/screens/chats/chats_screen.dart
@@ -37,7 +37,7 @@ class _ChatsScreenState extends State<ChatsScreen> {
         });
       },
       items: [
-        BottomNavigationBarItem(icon: Icon(Icons.messenger_outline_outlined), label: "Chats"),
+        BottomNavigationBarItem(icon: Icon(Icons.messenger), label: "Chats"),
         BottomNavigationBarItem(icon: Icon(Icons.people), label: "People"),
          BottomNavigationBarItem(icon: Icon(Icons.camera), label: "Status"),
         BottomNavigationBarItem(icon: Icon(Icons.call), label: "Calls"),
@@ -55,7 +55,7 @@ class _ChatsScreenState extends State<ChatsScreen> {
   AppBar buildAppBar() {
     return AppBar(
       automaticallyImplyLeading: false,
-      title: Text("Chats"),
+      title: Text("ChitChat"),
       actions: [
         IconButton(
           icon: Icon(Icons.search),
